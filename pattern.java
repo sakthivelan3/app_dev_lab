383e2f06011687d2c579457b247fa6a7642bab9c
Sakthivelan <kumaresansakthi007@gmail.com>
2024-04-15 18:36:47 +0530

    normal pyramid

--- pattern.java
+++ pattern.java
@@ -24,9 +24,28 @@ public class pattern {
         }
         
     }
+    public void pattern2(int n){
+        for(int i=0;i<n;i++){
+            //space
+            for(int j=0;j<n-i-1;j++){                               //Calculating the spaces in the pattern-> n-i-1
+                System.out.print(" ");
+            }
+            //star
+            for(int j=0;j<2*i+1;j++){
+                System.out.print("*");
+            } 
+            //Space
+            for(int j=0;j<n-i-1;j++){
+                System.out.print(" ");
+            }  
+            System.out.print("\n");                                //Calculating the stars in the pattern-> 2*i+1
+        }
+    }
     public static void main(String[] args) {
         pattern inp = new pattern();
         inp.pattern1(5);
+        System.out.println("\n");
+        inp.pattern2(6);
 
     }
 }
